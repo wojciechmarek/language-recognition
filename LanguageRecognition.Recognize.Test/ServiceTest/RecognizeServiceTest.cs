15f804ad34e7b494fc9fa6e82098bfd7794aede6
Wojciech Marek <marek.wojciech@outlook.com>
2019-04-07 21:26:34 +0200

    Added unit test for recognize module

---
+++ LanguageRecognition.Recognize.Test/ServiceTest/RecognizeServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using LanguageRecognition.Recognize.Interface;
+using LanguageRecognition.Recognize.ModuleException;
+using LanguageRecognition.Recognize.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageRecognition.Recognize.Test.ServiceTest
+{
+    [TestFixture]
+    class RecognizeServiceTest
+    {
+        [Test]
+        public void Service_WillObjectThrowExceptionWithEmptyPathWithModel_Exception()
+        {
+            IRecognizeService recognizeService = new RecognizeService();
+
+            Assert.Throws<InvalidTrainedAnnPathException>(() =>
+            {
+                recognizeService.GetPathOfTrainedAnn(string.Empty);
+            });
+        }
+
+        [Test]
+        public void Service_WillObjectThrowExceptionWithNullPathWithModel_Exception()
+        {
+            IRecognizeService recognizeService = new RecognizeService();
+
+            Assert.Throws<InvalidTrainedAnnPathException>(() =>
+            {
+                recognizeService.GetPathOfTrainedAnn(null);
+            });
+        }
+    }
+}
